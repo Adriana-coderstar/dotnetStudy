51edd9f802103c7bf7470d62d940aeba46bf4cde
Adriana <adrianam754@gmail.com>
2022-10-04 23:39:41 -0300

    feat: add initial deposit

--- Exercise/BankAccount/Program.cs
+++ Exercise/BankAccount/Program.cs
@@ -9,5 +9,10 @@ class Program {
 
     Console.Write("Will have initial deposit (y/n)? ");
     char answer = char.Parse(Console.ReadLine());
+
+    if ( answer == 'y' || answer == 'Y') {
+      Console.Write("Enter initial deposit amount: ");
+      double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    }
   }
 }
