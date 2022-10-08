222b8efc67a0a38c828bb226b76dd63ae15d3996
Adriana <adrianam754@gmail.com>
2022-10-07 23:42:42 -0300

    feat: add Program

--- Exercise/Employee/Program.cs
+++ Exercise/Employee/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using System.Collections.Generic;
+
+class Program {
+  static void Main(string[] args) {
+    Console.Write("How many employees will be registered? ");
+    int n = int.Parse(Console.ReadLine());
+
+    List<Employee> list = new List<Employee>();
+
+    for (int i = 1; i <= n; i++) {
+      Console.WriteLine($"Emplyoee #{i}: ");
+
+      Console.Write("Id: ");
+      int id = int.Parse(Console.ReadLine());
+
+      Console.Write("Name: ");
+      string name = Console.ReadLine();
+
+      Console.Write("Salary: ");
+      double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+      list.Add(new Employee(id, name, salary));
+
+      Console.WriteLine();
+    }
+  }
+}
