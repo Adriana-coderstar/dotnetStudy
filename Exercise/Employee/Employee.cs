8c1aa425b9dba62798bd101fae051307ea656131
Adriana <adrianam754@gmail.com>
2022-10-07 23:11:29 -0300

    feat: add constructor

--- Exercise/Employee/Employee.cs
+++ Exercise/Employee/Employee.cs
@@ -3,4 +3,10 @@ class Employee {
   public string Name { get; set; }
 
   public double Salary { get; private set;}
+
+  public Employee(int id, string name, double salary) {
+    id = id;
+    Name = name;
+    Salary = salary;
+  }
 }
