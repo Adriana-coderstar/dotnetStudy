51d21185973f4e14c7c4beeba1363d2737bf0ebe
Adriana <adrianam754@gmail.com>
2022-10-13 19:35:44 -0300

    feat: add readLine commands in the main program

--- Exercise/SalaryCalculation/Program.cs
+++ Exercise/SalaryCalculation/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Globalization;
+using System.Collections.Generic;
+using SalaryCalculation.Entities.Enums;
+
+namespace SalaryCalculation {
+  public class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Enter department's name: ");
+      Console.WriteLine("Enter worker data: ");
+
+      Console.Write("Name: ");
+      string name = Console.ReadLine();
+      
+      Console.Write("Level (Junior/Pleno/Senior): ");
+      WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+      Console.Write("Base salary: ");
+      double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    }
+  }
+}
