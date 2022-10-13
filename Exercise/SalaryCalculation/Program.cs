4de28be23633d2cd679034fcae5f45fa6730b2ff
Adriana <adrianam754@gmail.com>
2022-10-13 19:50:35 -0300

    feat:  add contract data

--- Exercise/SalaryCalculation/Program.cs
+++ Exercise/SalaryCalculation/Program.cs
@@ -1,6 +1,7 @@
 using System.Globalization;
 using System.Collections.Generic;
 using SalaryCalculation.Entities.Enums;
+using SalaryCalculation.Entities;
 
 namespace SalaryCalculation {
   public class Program {
@@ -17,6 +18,22 @@ namespace SalaryCalculation {
 
       Console.Write("Base salary: ");
       double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+      Console.Write("How many contracts to this worker? ");
+      int n = int.Parse(Console.ReadLine());
+
+      for (int i = 0; i <= n; i ++) {
+        Console.WriteLine($"Enter #{i} contract data: ");
+
+        Console.Write("Date (DD/MM/YYYY): ");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Value per hour: ");
+        double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.Write("Duration (hours): ");
+        int hours = int.Parse(Console.ReadLine());
+      }
     }
   }
 }
