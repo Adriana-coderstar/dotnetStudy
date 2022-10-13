a066ace9b9f21393da78d899fce10f3f72ff836a
Adriana <adrianam754@gmail.com>
2022-10-13 19:19:57 -0300

    feat: add constructor and properties

--- Exercise/SalaryCalculation/Entities/Worker.cs
+++ Exercise/SalaryCalculation/Entities/Worker.cs
@@ -6,6 +6,17 @@ namespace SalaryCalculation.Entities {
     public string Name { get; set; }
     public WorkerLevel Level { get; set; }
     public double BaseSalary { get; set; }
+
+    public Departament Departament { get; set; }
     public List<HourContract> Contracts { get; private set; } = new List<HourContract>();
+
+    public Worker() {}
+
+    public Worker(string name, WorkerLevel level, double baseSalary, Departament departament) {
+      Name = name;
+      Level = level;
+      BaseSalary = baseSalary;
+      Departament = departament;
+    }
   }
 }
