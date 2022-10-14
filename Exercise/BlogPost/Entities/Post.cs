511264643305798afefc7a14b460a443a02273f0
Adriana <adrianam754@gmail.com>
2022-10-14 20:34:49 -0300

    feat: add post class properties

---
+++ Exercise/BlogPost/Entities/Post.cs
@@ -0,0 +1,8 @@
+namespace BlogPost.Entities{
+  public class Post{
+    public DateTime Moment { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public int Likes { get; set; }
+  }
+}
