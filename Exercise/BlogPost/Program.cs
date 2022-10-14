8c72c545b14e23720336c47d695af06b2c72d5b1
Adriana <adrianam754@gmail.com>
2022-10-14 20:59:56 -0300

    feat: add main program and instance comments

--- Exercise/BlogPost/Program.cs
+++ Exercise/BlogPost/Program.cs
@@ -1,5 +1,10 @@
+using BlogPost.Entities;
+
 namespace BlogPost {
   public class Program {
-    
+    static void Main(string[] args) {
+      Comment c1 = new Comment("Have a nice trip!");
+      Comment c2 = new Comment("Wow that's awesome!");
+    }
   }
 }
