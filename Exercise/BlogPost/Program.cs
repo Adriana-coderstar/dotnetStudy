7d649c8e2c0d457a61e0eff0086faf66ac37a745
Adriana <adrianam754@gmail.com>
2022-10-14 21:05:31 -0300

    feat: add Post instance

--- Exercise/BlogPost/Program.cs
+++ Exercise/BlogPost/Program.cs
@@ -5,6 +5,12 @@ namespace BlogPost {
     static void Main(string[] args) {
       Comment c1 = new Comment("Have a nice trip!");
       Comment c2 = new Comment("Wow that's awesome!");
+
+      Post p1 = new Post(DateTime.Parse("14/10/2022"),
+        "Traveling to New Zealand",
+        "I'm going to visit this wonderful country!",
+        12
+      );
     }
   }
 }
