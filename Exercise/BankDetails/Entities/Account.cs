ea8b7620f1c47b4e43f7639a61c9dff859d2819a
Adriana <adrianam754@gmail.com>
2022-10-15 20:12:34 -0300

    feat: add constructor

--- Exercise/BankDetails/Entities/Account.cs
+++ Exercise/BankDetails/Entities/Account.cs
@@ -4,5 +4,11 @@ namespace BankDetails.Entities {
     public int Number { get; private set; }
     public string Holder { get; private set; }
     public double Balance { get; protected set; }
+    public Account() {}
+    public Account(int number, string holder, double balance) {
+      Number = number;
+      Holder = holder;
+      Balance = balance;
+    }
   }
 }
