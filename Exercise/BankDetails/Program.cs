6f68f5652c298f038d1c1f3190616f02aab5a1d3
Adriana <adrianam754@gmail.com>
2022-10-15 20:58:07 -0300

    feat: add main program

--- Exercise/BankDetails/Program.cs
+++ Exercise/BankDetails/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BankDetails.Entities;
+
+namespace BankDetails {
+  public class Program {
+    static void Main(string[] args) {
+      BusinessAccount account = new BusinessAccount(8050, "Bob Brown", 100.00, 500.00);
+
+      Console.WriteLine($"Number account:{account.Number}");
+      Console.WriteLine($"Holder: {account.Holder}");
+      Console.WriteLine($"Balance: {account.Balance}");
+      Console.WriteLine($"LoanLimit: {account.LoanLimit}");
+    }
+  }
+}
