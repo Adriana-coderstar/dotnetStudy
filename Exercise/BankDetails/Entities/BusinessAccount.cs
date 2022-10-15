032938382b362fd46dc4753f25bea6119df976c2
Adriana <adrianam754@gmail.com>
2022-10-15 20:26:44 -0300

    feat: add constructor with superclass Account property

--- Exercise/BankDetails/Entities/BusinessAccount.cs
+++ Exercise/BankDetails/Entities/BusinessAccount.cs
@@ -2,5 +2,12 @@
 namespace BankDetails.Entities {
   public class BusinessAccount : Account {
     public double LoanLimit { get; set; }
+
+    public BusinessAccount() {}
+    public BusinessAccount(int number, string holder, double balance, double loanLimit)
+    : base(number, holder, balance) {
+      
+      LoanLimit = loanLimit;
+    }
   }
 }
