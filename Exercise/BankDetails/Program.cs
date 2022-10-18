6a5b02134ac08c7b99eb9d7e3cf96a00abc656f6
Adriana <adrianam754@gmail.com>
2022-10-17 23:02:50 -0300

    feat: change Main Program

--- Exercise/BankDetails/Program.cs
+++ Exercise/BankDetails/Program.cs
@@ -3,12 +3,15 @@ using BankDetails.Entities;
 namespace BankDetails {
   public class Program {
     static void Main(string[] args) {
-      BusinessAccount account = new BusinessAccount(8050, "Bob Brown", 100.00, 500.00);
+      Account acc1 = new Account(8050, "Bob Brown", 500.0);
 
-      Console.WriteLine($"Number account:{account.Number}");
-      Console.WriteLine($"Holder: {account.Holder}");
-      Console.WriteLine($"Balance: {account.Balance}");
-      Console.WriteLine($"LoanLimit: {account.LoanLimit}");
+      Account acc2 = new SavingAccount(1040, "Anna", 500.0, 0.01);
+
+      acc1.Withdraw(10.00);
+      acc2.Withdraw(10.00);
+
+      Console.WriteLine($"Balance Account Acc1:{acc1.Balance}");
+      Console.WriteLine($"Balance Account Acc2: {acc2.Balance}");
     }
   }
 }
