08387e7c6d95fdd96f27fa0a0a5fcf3d6e89ebd2
Adriana <adrianam754@gmail.com>
2022-10-17 21:45:11 -0300

    feat: add class SavingAccount property and constructor

---
+++ Exercise/BankDetails/Entities/SavingAccount.cs
@@ -0,0 +1,13 @@
+namespace BankDetails.Entities {
+  public class SavingAccount: Account {
+    public double InterestRate { get; set; }      
+  
+    public SavingAccount() {}
+
+    public SavingAccount(int number, string holder, double balance, double interestRate)
+    : base(number, holder, balance) {
+      InterestRate = interestRate;
+    }
+  }
+
+}
