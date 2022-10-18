62b0a4f7f92afc4148e41d5a422a2a8c548d8245
Adriana <adrianam754@gmail.com>
2022-10-17 23:01:52 -0300

    feat: add Withdraw method with superclass method reference

--- Exercise/BankDetails/Entities/SavingAccount.cs
+++ Exercise/BankDetails/Entities/SavingAccount.cs
@@ -12,5 +12,10 @@ namespace BankDetails.Entities {
     public void UpdateBalace() {
       Balance += Balance * InterestRate;
     }
+
+    public override void Withdraw(double amount) {
+      base.Withdraw(amount);
+      Balance -= 2.0;
+    }
   }
 }
